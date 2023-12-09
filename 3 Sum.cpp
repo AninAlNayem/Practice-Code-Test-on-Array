7a73fa6fd9071b0c68f14103092296ce566a7245
Nayem1503019 <106160241+Nayem1503019@users.noreply.github.com>
2023-12-09 20:51:39 +0600

    Create 3 Sum.cpp

---
+++ 3 Sum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+   int n;scanf("%d",&n);
+   int arr[n];
+   for(int i=0;i<n;i++){
+       scanf("%d",&arr[i]);
+   }
+   sort(arr,arr+n);
+
+   int cnt=0;
+
+   for(int i=0;i<n-2;i++){
+      int j=i+1,k=n-1;
+      while(j<k){
+        int sum=arr[i]+arr[j]+arr[k];
+        if(sum==0){
+            cnt++;
+            j++,k--;
+        }
+        else if(sum<0){
+            j++;
+        }
+        else{
+            k--;
+        }
+      }
+   }
+   printf("%d\n",cnt);
+
+return 0;
+}
